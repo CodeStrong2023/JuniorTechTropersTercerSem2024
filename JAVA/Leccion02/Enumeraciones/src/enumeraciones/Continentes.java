e648a321bbd7ab78c686dd1a822da5c02f798f31
Marianela Purretta <marianela.purretta@yahoo.com>
2024-04-13 18:48:34 -0300

    Clase02 JAVA-MARI/Proyecto: ENUMERACIONES

---
+++ JAVA/Leccion02/Enumeraciones/src/enumeraciones/Continentes.java
@@ -0,0 +1,29 @@
+
+package enumeraciones;
+
+public enum Continentes {
+    AFRICA(53, "1.2 billones"),// podemos agregar más a tributos
+    EUROPA(46, "1.1 billones"),
+    ASIA(44, "1.9 millones"),
+    AMERICA(34, "150.2 millones"),
+    OCEANIA(14, "1.2 billones");
+    
+    //Atributos:
+    private final int paises;
+    private String habitantes;
+    
+    //Creamos el constructor
+    Continentes(int paises, String habitantes){
+        this.paises = paises;
+        this.habitantes = habitantes;
+    }
+    
+    //Creamos el método get ya que al ser tipo private no podemos acceder
+    
+    public int getPaises(){
+        return this.paises;
+    }
+    public String getHabitantes(){
+        return this.habitantes;
+    }
+}
---
+++ JAVA/Leccion02/Enumeraciones/src/enumeraciones/Dias.java
@@ -0,0 +1,14 @@
+
+package enumeraciones;
+
+/*Cambiamosa class por enum, los elem de la enum son publicos, 
+estaticos y finales (todos Constantes)*/
+public enum Dias {
+    LUNES,
+    MARTES, 
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO        
+}
