7564a8ee22e25b878fdc938790b5a8f45a78c40b
Mauro <maurobarroso1999@gmail.com>
2024-04-17 16:03:21 -0300

    Video 1.2 Recorremos los elementos de un arreglo

--- JAVASCRIPT/TSLeccion02/02-03-arreglos.js
+++ JAVASCRIPT/TSLeccion02/02-03-arreglos.js
@@ -4,3 +4,10 @@
 const autos = ['Ferrari', 'Ranault', 'BMW'] //Usamos const porque la referencia ya no se va a modificar
 console.log(autos)
 
+//Recorremos los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
+
+for(let i = 0; i < autos.length; i++){
+    console.log(i+' : '+autos[i]);
+}
