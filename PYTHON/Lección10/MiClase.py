d4901dcc2995cda521bd64c85c4b7d3a75452251
Lassa <nelsonlassa63@gmail.com>
2024-04-23 20:23:24 -0300

    Variables de clase Leccion 10

---
+++ PYTHON/Lección10/MiClase.py
@@ -0,0 +1,15 @@
+class MiClase:
+    #  Variable de clase, este atributo dará a cada objeto el mismo valor
+    variable_clase = 'Esta es una variable de clase'
+
+    def __init__(self, variable_instancia):  # La variable de instancia, da diferentes valores
+        self.variable_instancia = variable_instancia
+
+
+print(MiClase.variable_clase)
+miCalse1 = MiClase('Esta es una variable de instancia')
+print(miCalse1.variable_instancia)
+print(miCalse1.variable_clase)
+miCalse2= MiClase('Esta es otra prueba de variable de instancia')
+print(miCalse2.variable_instancia)
+print(miCalse2.variable_clase)
