171db1aa09ad330cd1b24910fc13ee2a8274cb5e
Franco <francopaganoo2015@gmail.com>
2024-04-24 00:31:54 -0300

    Video 1.3_Modificar Arreglo y 1.4_Preguntar si es Arreglo JS

--- JAVASCRIPT/TSLeccion02/02-03-arreglos.js
+++ JAVASCRIPT/TSLeccion02/02-03-arreglos.js
@@ -11,3 +11,24 @@ console.log(autos[2]);
 for(let i = 0; i < autos.length; i++){
     console.log(i+' : '+autos[i]);
 }
+
+// Modificamos los elementos del arreglo
+autos[1] = "Volvo";
+console.log(autos[1]);
+
+// Agregar nuevos valores al arreglo
+autos.push("Audi"); // Este nuevo elementos se agrega al final
+console.log(autos);
+
+// Otra forma de agregar elementos
+autos[autos.length] = "Proche";
+console.log(autos);
+
+//Tercera forma de agreghar elementos teniendo CUIDADO
+autos[6] = "Renault";
+console.log(autos);
+
+// Como preguntar si es una Array o Arreglo
+console.log(Array.isArray(autos)); // Devolvera un booleano
+
+console.log(autos instanceof Array); // Pregunta si la variable es una instancia de la clase Array
