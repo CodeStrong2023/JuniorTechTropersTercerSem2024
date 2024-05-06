331a3f3128396976a4608067b1f18270118e976d
Valentino <jostar843@gmail.com>
2024-05-06 03:19:58 -0300

    Clase 4 Java Ejercicio con instanceof Parte 1 y 2

--- JAVA/Leccion04/instanceOf/src/main/java/test/TestInstanceOf.java
+++ JAVA/Leccion04/instanceOf/src/main/java/test/TestInstanceOf.java
@@ -6,18 +6,23 @@ import domain.Gerente;
 public class TestInstanceOf {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Juan", 1000);
-        //determinarTipo(empleado1);
-        empleado1 = new Gerente("Nelson", 2000, "Sistema");
         determinarTipo(empleado1);
+        empleado1 = new Gerente("Nelson", 2000, "Sistema");
+        //determinarTipo(empleado1);
     }
 
     //Polimorfismo
     public static void determinarTipo(Empleado empleado) {
         if (empleado instanceof Gerente) {
             System.out.println("Es de tipo Gerente");
+            //Gerente gerente = (Gerente)empleado;
+            //((Gerente) empleado).getDepartamento();
+            System.out.println("gerente ="+gerente.getDepartamento());
         }
         else if (empleado instanceof Empleado) {
             System.out.println("Es de tipo empleado");
+            //Gerente gerente = (Gerente)empleado;
+            //System.out.println("gerente ="+gerente.getDepartamento());
         }
         else if (empleado instanceof Object) {
             System.out.println("Es de tipo Object");
