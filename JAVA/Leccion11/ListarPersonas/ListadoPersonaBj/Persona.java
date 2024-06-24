2df10176f784d27b0d7c1f0bb81599d13c5b496a
Marianela <135768183+MarianelaPurretta@users.noreply.github.com>
2024-06-24 15:30:06 -0300

    Clase10|JAVA|ListarPersonas|Marianela

---
+++ JAVA/Leccion11/ListarPersonas/ListadoPersonaBj/Persona.java
@@ -0,0 +1,22 @@
+public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    //Constructor vacío:
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    //Costructor con parámetros (sobrecarga de constructores):
+    public Persona(String nombre, String tel, String email){
+        this();
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+
+
+}
