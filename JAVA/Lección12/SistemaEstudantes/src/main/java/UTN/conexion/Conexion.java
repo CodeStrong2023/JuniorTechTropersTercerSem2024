422d931dba026991cf8d019624991ab601ab6afd
Lassa <nelsonlassa63@gmail.com>
2024-06-30 21:35:23 -0300

    Clase 12 Java Proyecto Sistema Estudiantes, packege dato, archivo EstudiantesDAO

--- JAVA/Lección12/SistemaEstudantes/src/main/java/UTN/conexion/Conexion.java
+++ JAVA/Lección12/SistemaEstudantes/src/main/java/UTN/conexion/Conexion.java
@@ -11,7 +11,7 @@ public class Conexion {
         Connection connexion = null;
         // Variables para conectarnos a la base de datos
         //var baseDatos ="estudiantes2024";
-        var baseDatos = "estudiantes2022"; // Por si la hicieron con el año del profe
+        var baseDatos = "estudiantes"; // Por si la hicieron con el año del profe
         var url = "jdbc:mysql://localhost:3306/" + baseDatos;
         var usuario = "root";
         //var password = "tu_clave";
